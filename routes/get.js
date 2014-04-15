d747959586315314aefd58e99fa62cba02690de0
Troy <nztrooy@gmail.com>
2014-04-15 20:55:27 +1200

    added a question

--- routes/get.js
+++ routes/get.js
@@ -96,9 +96,9 @@ exports.question = function(req, res){
     var e = 1;
 
     var qt = "[2 marks] What is the output of the following code?" +
-        '\n\t String a = new String("ab");' +
-        '\n\t String b = new String("ab");' +
-        '\n\t String c = new String("a");' +
+        '\n\t String a = new String(\"ab\");' +
+        '\n\t String b = new String(\"ab\");' +
+        '\n\t String c = new String(\"a\");' +
         '\n\t String d = b;' +
         '\n\n\t System.out.println(a == d);' +
         '\n\t System.out.println(b.equals(c));' +
